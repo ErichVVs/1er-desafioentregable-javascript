9d052401549c4df5b2b539ec3824639203f8d360
erichvvs <erich.adan@gmail.com>
2022-07-22 20:52:36 -0500

    let y const

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-function datos(maximo_calorias, entrada, consumo){
+/*function datos(maximo_calorias, entrada, consumo){
     this.maximo_calorias = maximo_calorias;
     this.entrada = entrada;
     this.consumo = consumo
@@ -20,4 +20,76 @@ function datos(maximo_calorias, entrada, consumo){
 }
 
 const miscalorias = new datos (parseFloat(prompt("Ingrese el límite de calorías de tu plan alimenticio:"), "", 0));
-console.log(miscalorias);
+console.log(miscalorias);*/
+
+/*
+let numeroA = 5;
+let numeroB = 10;
+const NUMEROC = 3;
+
+let suma = numeroA + numeroB;
+alert (suma);
+
+let resta = numeroB - numeroA;
+alert (resta);
+
+let multiplicacion = numeroA * numeroB;
+alert (multiplicacion); 
+
+let division = numeroB / numeroA;
+alert (division);
+
+const suma = numeroA + numeroB;
+alert (suma);
+
+const resta = numeroB - numeroA;
+alert (resta);
+
+const multiplicacion = numeroB * numeroA;
+alert (multiplicacion);
+
+const division = numeroB / numeroA;
+alert (division);
+
+const division = numeroA / numeroB;
+alert (division);
+
+let multiplicacion = numeroB * numeroA * NUMEROC;
+alert (multiplicacion);*/
+
+/*let textoA = "CODER";
+let textoB = "HOUSE";
+const BLANCO = " ";
+
+let resultadoA = textoA + BLANCO + textoB;
+alert (resultadoA);
+
+let resultadoB = textoB + 41;
+alert (resultadoB);
+
+let resultadoC = textoA + 41;
+console.log (resultadoC);
+
+let nombreIngresado = prompt ("Ingrese su nombre");
+alert (nombreIngresado);
+
+const nombreIngresado = prompt ("Ingrese su nomre:")
+alert (nombreIngresado);
+
+console.log ("Mensaje de prueba");
+
+let nombre = "Erich";
+console.log (nombre);
+
+let entrada = prompt("Ingrese una letra:");
+let salida = ("Tu letra ingresada es:" + " " + entrada);
+alert(salida);*/
+
+let creditoDisponible = prompt("Ingresa tu credito disponible:");
+let dineroDisponible = prompt("Inresa tu dinero dispobible");
+
+const resta = (creditoDisponible - dineroDisponible);
+alert (`El credito no puede disminuir por la cantidad de: \$${resta} pesos.`);
+
+
+
