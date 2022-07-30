4075717fc998e0136e66aa5923dc6044bef37c29
erichvvs <erich.adan@gmail.com>
2022-07-29 22:23:07 -0500

    primer repaso de los arrays

--- main2.js
+++ main2.js
@@ -158,3 +158,11 @@ while(entrada != "esc"){
 }
 */
 
+const numeros = [1,2,3,4,5];
+
+console.log ( numeros[0]);
+console.log ( numeros[3]);
+
+let resultado = numeros[1] + numeros[2];
+console.log (resultado);
+
